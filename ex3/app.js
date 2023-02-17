6e843aed2945fa92e888a4662804940967060233
Ma7hs <Matheus.siqueira100@gmail.com>
2023-02-17 11:20:34 -0300

    commiting

--- ex3/app.js
+++ ex3/app.js
@@ -18,15 +18,29 @@ console.log("BEM VINDO A VERIFICADOR DE NUMEROS")
 
 dataInput.question('Insira o número inicial: ', function(numero1){
     numeroInicial = Number(numero1);
-
-    dataInput.question('Insira o número final: ', function(numero2){
-        numeroFinal = Number(numero2);
-
-        dataInput.question('Escolha entre PAR ou IMPAR: ', function(decisao){
-            escolha = String(decisao.toUpperCase());
-
-            tabuada = verificaTabuada.verificaImparPar(Number(numeroInicial), Number(numeroFinal), escolha)
-            dataInput.close();
-        })  
-    })
+    if(numeroInicial > 500 || numeroInicial < 0){
+        console.log('ERROR: Digite um número que seja entre 0 e 500')
+        dataInput.close();
+    }else{
+        dataInput.question('Insira o número final: ', function(numero2){
+            numeroFinal = Number(numero2);
+            if(numeroFinal > 100 || numeroFinal < 1000){
+                console.log('ERROR: Digite um número que seja entre 100 a 1000')
+                dataInput.close();
+            }else if(numeroInicial == numeroFinal){
+                console.log('ERROR: As entradas não pode ser iguais!')
+                dataInput.close();
+            }else if(numeroInicial == '' || numeroFinal == ''){
+                console.log('ERROR: As entradas não podem estar vazias')
+                dataInput.close();
+            }else{
+                dataInput.question('Escolha entre PAR ou IMPAR: ', function(decisao){
+                    escolha = String(decisao.toUpperCase());
+        
+                    tabuada = verificaTabuada.verificaImparPar(Number(numeroInicial), Number(numeroFinal), escolha)
+                    dataInput.close();
+                })  
+            }
+        })
+    }    
 })
